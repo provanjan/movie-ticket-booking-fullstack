e10147f69d813888892f2501563a5a69152310fe
provanjan <102207430+provanjan@users.noreply.github.com>
2022-04-20 20:24:58 +0530

    Delete src directory

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import errorReducer from "./errorReducer";
-import ticketBookingReducer from "./ticketBookingReducer";
-
-export default combineReducers({
-  errors: errorReducer,
-  ticketBookings: ticketBookingReducer,
-});
